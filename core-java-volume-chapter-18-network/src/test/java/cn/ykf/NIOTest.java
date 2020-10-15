71ee4001b6450ad8435cb6314a65ffb4f94607af
yukaifan <1092882580@qq.com>
2020-10-15 10:48:44 +0800

    feat(): nio - Buffer

--- core-java-volume-chapter-18-network/src/test/java/cn/ykf/NIOTest.java
+++ core-java-volume-chapter-18-network/src/test/java/cn/ykf/NIOTest.java
@@ -19,6 +19,57 @@ public class NIOTest {
     private final int DEFAULT_BUFFER_SIZE = 1024;
 
     /**
+     * 核心方法，核心属性
+     */
+    @Test
+    public void testBuffer() {
+        final ByteBuffer buffer = ByteBuffer.allocate(1024);
+        System.out.println("-------------allocate()----------------");
+        System.out.println(buffer.position());
+        System.out.println(buffer.limit());
+        System.out.println(buffer.capacity());
+
+        // 写数据
+        buffer.put("abcde".getBytes());
+        System.out.println("-------------put()----------------");
+        System.out.println(buffer.position());
+        System.out.println(buffer.limit());
+        System.out.println(buffer.capacity());
+
+        // 切换到读模式
+        buffer.flip();
+        System.out.println("-------------flip()----------------");
+        System.out.println(buffer.position());
+        System.out.println(buffer.limit());
+        System.out.println(buffer.capacity());
+
+        // 读数据
+        final byte[] dst = new byte[buffer.limit()];
+        buffer.get(dst);
+        System.out.println("-------------get()----------------");
+        System.out.println(new String(dst));
+        System.out.println(buffer.position());
+        System.out.println(buffer.limit());
+        System.out.println(buffer.capacity());
+
+        // 重读
+        buffer.rewind();
+        System.out.println("-------------rewind()----------------");
+        System.out.println(buffer.position());
+        System.out.println(buffer.limit());
+        System.out.println(buffer.capacity());
+
+        // 清空
+        buffer.clear();
+        System.out.println("-------------clear()----------------");
+        System.out.println(buffer.position());
+        System.out.println(buffer.limit());
+        System.out.println(buffer.capacity());
+        // 数据还在
+        System.out.println((char) buffer.get());
+    }
+
+    /**
      * 简单的例子，将原本的文件字节流和随机访问流转成channel进行读写操作
      */
     @Test
