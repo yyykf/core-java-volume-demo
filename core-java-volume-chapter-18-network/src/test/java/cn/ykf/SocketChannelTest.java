4d654572b73c87c7fe1a69aba42d45ce7938e7b7
yukaifan <1092882580@qq.com>
2020-10-16 09:38:49 +0800

    feat(): nio - SocketChannel&ServerSocketChannel（Non-Blocking）

--- core-java-volume-chapter-18-network/src/test/java/cn/ykf/SocketChannelTest.java
+++ core-java-volume-chapter-18-network/src/test/java/cn/ykf/SocketChannelTest.java
@@ -5,11 +5,12 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.util.Iterator;
+import java.util.Scanner;
 
 /**
  * @author YuKaiFan <1092882580@qq.com>
@@ -75,6 +76,85 @@ public class SocketChannelTest {
                 System.out.println(new String(buffer.array(), 0, len));
                 buffer.clear();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 非阻塞
+     */
+    @Test
+    public void serverWithNonBlocking() {
+        try (final ServerSocketChannel serverSocket = ServerSocketChannel.open()) {
+            // 监听端口
+            serverSocket.bind(new InetSocketAddress(9999));
+            // 切换非阻塞模式
+            serverSocket.configureBlocking(false);
+
+            // 注册到选择器
+            final Selector selector = Selector.open();
+            // 监听接受事件
+            serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+
+            // 轮询获取选择器上已经准备就绪的事件
+            // select 依旧是阻塞的
+            while (selector.select() > 0) {
+                // 已经准备就绪的事件
+                final Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+
+                while (iterator.hasNext()) {
+                    // 判断分别是什么事件就绪
+                    final SelectionKey key = iterator.next();
+
+                    if (key.isAcceptable()) {
+                        // 接受就绪
+                        final SocketChannel socket = serverSocket.accept();
+                        // 获取客户端连接并且配置为非阻塞
+                        socket.configureBlocking(false);
+                        // 也注册到选择器上，这里选择监听读事件
+                        socket.register(selector, SelectionKey.OP_READ);
+                    } else if (key.isReadable()) {
+                        // 读就绪
+                        final SocketChannel socket = (SocketChannel) key.channel();
+                        // 开始读数据
+                        final ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+                        int len;
+                        while ((len = socket.read(buffer)) > 0) {
+                            System.out.println(len);
+                            buffer.flip();
+                            System.out.println(new String(buffer.array(), 0, len));
+                            buffer.clear();
+                        }
+                    }
+
+                    // 已就绪事件处理完，需要移除
+                    iterator.remove();
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void clientWithNonBlocking() {
+        try (final SocketChannel socket = SocketChannel.open(new InetSocketAddress("localhost", 9999));
+             final Scanner in = new Scanner(System.in)) {
+            // 配置非阻塞模式
+            socket.configureBlocking(false);
+
+            final ByteBuffer buffer = ByteBuffer.allocate(1024);
+            // 给服务器发送数据
+            while (in.hasNextLine()) {
+                buffer.put((LocalDateTime.now().toString() + "\n" + in.nextLine()).getBytes());
+                buffer.flip();
+                socket.write(buffer);
+                buffer.clear();
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
